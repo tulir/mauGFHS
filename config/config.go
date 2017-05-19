bf67d90595d3312029eede096c6cca01a5fc16b2
Tulir Asokan <tulir@maunium.net>
2017-05-19 12:36:05 +0300

    Add logging config section

--- config/config.go
+++ config/config.go
@@ -1,11 +1,45 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"maunium.net/go/maulogger"
+
+	"gopkg.in/yaml.v2"
+)
 
 // Config is the base configuration container.
 type Config struct {
 	Database  DBConfig         `yaml:"database"`
 	Addresses []ListenLocation `yaml:"listen"`
+	Logging   LogConfig        `yaml:"logging"`
+}
+
+// LogConfig contains logging configurations
+type LogConfig struct {
+	Directory      string `yaml:"directory"`
+	FileNameFormat string `yaml:"file-name-format"`
+	FileMode       uint32 `yaml:"file-mode"`
+}
+
+// GetFileFormat returns a mauLogger-compatible logger file format based on the data in the struct.
+func (lc LogConfig) GetFileFormat() maulogger.LoggerFileFormat {
+	path := lc.FileNameFormat
+	if len(lc.Directory) > 0 {
+		path = lc.Directory + "/" + path
+	}
+
+	return func(now string, i int) string {
+		return fmt.Sprintf(path, now, i)
+	}
+}
+
+// Configure configures a mauLogger instance with the data in this struct.
+func (lc LogConfig) Configure(log *maulogger.Logger) {
+	log.FileFormat = lc.GetFileFormat()
+	log.FileMode = os.FileMode(lc.FileMode)
 }
 
 // ListenLocation is a location where the server should listen.
@@ -27,4 +61,20 @@ type DBConfig struct {
 // GetDSN gets the SQL data source name for this database config.
 func (config DBConfig) GetDSN() string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.Username, config.Password, config.Address, config.Port, config.Database)
+}
+
+var config Config
+
+// GetConfig returns the singleton Config instance.
+func GetConfig() Config {
+	return config
+}
+
+// Open opens the config at the given path.
+func Open(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(data, &config)
 }
