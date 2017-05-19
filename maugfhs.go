21951c584a527bcf37d6bd1043b9cbe1c5a1f541
Tulir Asokan <tulir@maunium.net>
2017-05-19 12:50:40 +0300

    Add more initialization steps

--- maugfhs.go
+++ maugfhs.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"maunium.net/go/mauGFHS/config"
 	"maunium.net/go/mauGFHS/db"
+	flag "maunium.net/go/mauflag"
+	log "maunium.net/go/maulogger"
 )
 
+var configPath = flag.MakeFull("c", "config", "The path to the config file", "config.yml").String()
+var debug = flag.MakeFull("d", "debug", "Whether or not to enable debug mode", "false").Bool()
+var wantHelp, _ = flag.MakeHelpFlag()
+
 func main() {
-	err := db.Open("root", "password", "localhost", 3306, "maugfhs")
-	if err != nil {
-		panic(err)
+	flag.SetHelpTitles("mauGFHS 0.1 - A server that can serve as a backend for many kinds of services that only require file hosting.", "mauGFHS [-c /path/to/config] [-d] [-h]")
+	err := flag.Parse()
+	if err != nil || *wantHelp {
+		flag.PrintHelp()
+		return
 	}
 
-	db.CreateTables()
-	err = db.Close()
+	err = config.Open(*configPath)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Failed to open config: %v\n", err)
+		if *debug {
+			panic(err)
+		}
+		os.Exit(1)
 	}
+
+	config.GetConfig().Logging.Configure(log.DefaultLogger)
+	if *debug {
+		log.DefaultLogger.PrintLevel = log.LevelDebug.Severity
+	}
+	log.Debugln("Logging initialized.")
+
+	err = db.Open(config.GetConfig().Database)
+	if err != nil {
+		log.Fatalf("Failed to open database connection: %v\n", err)
+		if *debug {
+			panic(err)
+		}
+		os.Exit(1)
+	}
+	db.CreateTables()
+
 }
